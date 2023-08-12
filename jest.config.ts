ab13ba09315ae5d9eedbf12df04c1de89b674f6e
Carson Wah <carsonwah@gmail.com>
2023-08-12 13:48:05 +0800

    Fix jest

--- jest.config.ts
+++ jest.config.ts
@@ -8,8 +8,8 @@ const config: Config = {
   testEnvironment: 'jsdom', // Enable browser-like environment e.g. having "document" variable
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "identity-obj-proxy"
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.ts",
+    "\\.(css|scss|sass|less)$": "identity-obj-proxy"
   },
 };
 

4572e1fa99c073a750f207b3f137fca8d62cd500
Carson Wah <carsonwah@gmail.com>
2023-08-15 00:39:26 +0800

    Add airbnb eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/electron',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'jest'],
+  rules: {
+    'linebreak-style': 0,
+  },
+};
